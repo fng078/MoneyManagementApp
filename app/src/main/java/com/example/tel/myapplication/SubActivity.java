e794a603dd8fb6863114f9d917df12c55b0a7e53
TEL <c501737033@tokushima-u.ac.jp>
2018-06-03 16:00:39 +0900

    画面遷移実装

--- app/src/main/java/com/example/tel/myapplication/SubActivity.java
+++ app/src/main/java/com/example/tel/myapplication/SubActivity.java
@@ -1,24 +1,22 @@
 package com.example.tel.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
-        Button sendButton = findViewById(R.id.button_Income);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        Button returnButton = findViewById(R.id.button_decision);
+        returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), SubActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
