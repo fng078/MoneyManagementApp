ccf2dff1a19b12efd489f0604e1ff44d96ba6b0b
TEL <c501737033@tokushima-u.ac.jp>
2018-06-05 01:11:17 +0900

    SubActivity からMainActivity へデータ返却

--- app/src/main/java/com/example/tel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tel/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tel.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,17 +24,35 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("backgroundColor", 1);
-                startActivity(intent);
+                startActivityForResult(intent, 100);
             }
         });
+
         spendingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent;
                 intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("backgroundColor", 2);
-                startActivity(intent);
+                startActivityForResult(intent, 200);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100){
+            int  inout_income_result = Integer.valueOf(data.getStringExtra("result"));
+            Log.d("result", String.valueOf(inout_income_result));
+        }
+        else if(requestCode == 200){
+            int  inout_spending_result = Integer.valueOf(data.getStringExtra("result"));
+            Log.d("result", String.valueOf(inout_spending_result));
+        }
+        else{
+            // TODO: エラー処理
+
+        }
+    }
+
 }
